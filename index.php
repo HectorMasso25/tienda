8758a37c1093331b937593e9e54f6b83d60e1c60
HectorMasso25 <63612646+HectorMasso25@users.noreply.github.com>
2021-10-01 12:37:02 -0500

    Base de ayuda
    Usé como base un Crud de biblioteca que ya tenia hecho.

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<title>Tiendas</title>
+	</head>	
+	<body>
+		<header>
+			Bienvenido
+		</header>
+        
+		<table border=1>			
+			<tr>
+				<td><a href="ingresar.php">Ingresar Tienda</a></td>
+			</tr>
+			<tr>
+				<td><a href="mostrar.php">Ver Tiendas</a></td>
+			</tr>
+		</table>
+		
+	</body>
+</html>
